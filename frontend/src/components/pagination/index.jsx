d97c67dfb7eb32f2c41838957256742d2dc3b736
vineeth-krishna-1234 <77538493+vineeth-krishna-1234@users.noreply.github.com>
2025-03-21 23:20:46 +0530

    feat : pagination

---
+++ frontend/src/components/pagination/index.jsx
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { setFilters, fetchTodos } from "../../redux/slices/todo";
+
+const PaginationComponent = () => {
+  const dispatch = useDispatch();
+  const page = useSelector((state) => state.todo.filters.page);
+  return (
+    <div>
+      <div class="flex">
+        <a
+          onClick={() => {
+            if (page) {
+              dispatch(setFilters({ page: page - 1 }));
+              dispatch(fetchTodos());
+            }
+          }}
+          href="#"
+          class="flex items-center justify-center px-4 h-10 me-3 text-base font-medium text-gray-500 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+        >
+          <svg
+            class="w-3.5 h-3.5 me-2 rtl:rotate-180"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 14 10"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M13 5H1m0 0 4 4M1 5l4-4"
+            />
+          </svg>
+          Previous
+        </a>
+        <a
+          href="#"
+          class="flex items-center justify-center px-4 h-10 text-base font-medium text-gray-500 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+          onClick={() => {
+            dispatch(setFilters({ page: page + 1 }));
+            dispatch(fetchTodos());
+          }}
+        >
+          Next
+          <svg
+            class="w-3.5 h-3.5 ms-2 rtl:rotate-180"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 14 10"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M1 5h12m0 0L9 1m4 4L9 9"
+            />
+          </svg>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default PaginationComponent;
